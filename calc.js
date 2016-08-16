f62410aea532231ae4c8764416c6b54420985d4d
Victor Tsang <victorwltsang@gmail.com>
2016-08-15 20:42:28 -0700

    a not so functional calculator

---
+++ calc.js
@@ -0,0 +1,163 @@
+
+    var numArray = [];
+    var count = 0;
+    var result = $("#result");
+    $(document).ready(function() {
+
+        result.html("0");
+
+    });
+
+    $(".calcBtn").on("click", function() {
+        console.log($(this).text());
+
+        if ($(this).text() == "AC") {
+
+            numArray = [];
+            result.html("0");
+        }
+        if ($(this).text() == "CE") {
+
+            numArray.pop();
+            result.html(numArray);
+        }
+
+        if ($(this).text() == "=") {
+              numArray.push($(this).text());
+            count = 2;
+
+            countModifiers($(this).text());
+            numArray.pop();
+            result.html(numArray);
+            console.log("Count is " + count);
+        }
+
+        if ($(this).text() !== "CE" && $(this).text() !== "AC" && $(this).text() !== "=") {
+            console.log("i dont want ac "+ $(this).text());
+            numArray.push($(this).text());
+            countModifiers($(this).text());
+            result.html(numArray);
+            console.log("Count is " + count);
+        }
+
+    });
+
+    // need fixing on the if search on modifier, "+" will always happen first
+    function countModifiers(num) {
+
+        if (num.match(/[\+*-/%]/g) ) {
+            count++;
+            console.log("Count is " + count);
+
+        }
+
+        if (count === 2) {
+            count = 1;
+            console.log(numArray);
+            if (numArray.join("").match(/\+/)) {
+                console.log("Match first +");
+                addition();
+            }else if (numArray.join("").match(/\-/)) {
+                console.log("Match first -");
+                subtraction();
+            }
+            else if (numArray.join("").match(/\*/)) {
+                console.log("Match first *");
+                multiplication();
+            }else if (numArray.join("").match(/\//)) {
+                console.log("Match first /");
+                division();
+            }else if (numArray.join("").match(/%/)) {
+                console.log("Match first %");
+                modulo();
+            }
+
+
+        }
+
+    }
+
+    function addition() {
+        var lastMod = numArray.pop();
+        var temp = numArray.join("").replace(/[\+*-/%]/g, ",").split(",").map(Number);
+        var action = temp.reduce(function(sum, item) {
+
+            console.log(parseFloat(sum) + "+" + parseFloat(item));
+            return parseFloat(sum) + parseFloat(item);
+
+        });
+        console.log(action);
+
+        numArray = [];
+        numArray.push(action);
+        numArray.push(lastMod);
+        result.html(numArray);
+    }
+
+    function subtraction() {
+        var lastMod = numArray.pop();
+        var temp = numArray.join("").replace(/[\+*-/%]/g, ",").split(",").map(Number);
+        var action = temp.reduce(function(sum, item) {
+
+            console.log(parseFloat(sum) + " - " + parseFloat(item));
+            return parseFloat(sum) - parseFloat(item);
+
+        });
+        console.log(action);
+
+        numArray = [];
+        numArray.push(action);
+        numArray.push(lastMod);
+        result.html(numArray);
+    }
+
+    function multiplication() {
+        var lastMod = numArray.pop();
+        var temp = numArray.join("").replace(/[\+*-/%]/g, ",").split(",").map(Number);
+        var action = temp.reduce(function(sum, item) {
+
+            console.log(parseFloat(sum) + " * " + parseFloat(item));
+            return parseFloat(sum) * parseFloat(item);
+
+        });
+        console.log(action);
+
+        numArray = [];
+        numArray.push(action);
+        numArray.push(lastMod);
+        result.html(numArray);
+    }
+
+    function division() {
+        var lastMod = numArray.pop();
+        var temp = numArray.join("").replace(/[\+*-/%]/g, ",").split(",").map(Number);
+        var action = temp.reduce(function(sum, item) {
+
+            console.log(parseFloat(sum) + " / " +parseFloat(item));
+            return (parseFloat(sum) / parseFloat(item)).toFixed(10);
+
+        });
+        console.log(action);
+
+        numArray = [];
+        numArray.push(action);
+        numArray.push(lastMod);
+        result.html(numArray);
+    }
+
+    function modulo() {
+        var lastMod = numArray.pop();
+        var temp = numArray.join("").replace(/[\+*-/%]/g, ",").split(",").map(Number);
+        var action = temp.reduce(function(sum, item) {
+
+            console.log(parseFloat(sum) + " % " + parseFloat(item));
+            return parseFloat(sum) % parseFloat(item);
+
+        });
+        console.log(action);
+
+        numArray = [];
+        numArray.push(action);
+        numArray.push(lastMod);
+        result.html(numArray);
+    }
